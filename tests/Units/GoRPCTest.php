58b8e9540a10db24014ae5b3be286bd061fbdd87
李铭昕 <715557344@qq.com>
2018-04-19 10:40:36 +0800

    增加测试

---
+++ tests/Units/GoRPCTest.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | 基础测试类 [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 limingxinleo All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <715557344@qq.com> <https://github.com/limingxinleo>
+// +----------------------------------------------------------------------
+namespace Tests\Units;
+
+use App\Common\Clients\Rpc\BasicClient;
+use Tests\UnitTestCase;
+
+/**
+ * Class UnitTest
+ */
+class GoRPCTest extends UnitTestCase
+{
+    public function testVersionCase()
+    {
+        $version = BasicClient::getInstance()->version();
+        $this->assertEquals('1.0.0', $version);
+    }
+}
